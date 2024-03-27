e4757d1e5718575a327914d6aa2f28f2db781be2
kstrwind <kstrwind@163.com>
2024-03-27 10:44:04 +0800

    fix

---
+++ web/vue/chapter/02_tab_control/src/components/TabControl.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="tab-control">
+    <div v-for="(title, index) in titles" 
+		class="tab-control-item"
+		:class="{active: currentIndex === index}"
+		:key="title"
+		@click="itemClick(index)">
+		<span>{{title}}</span>
+	</div>
+  </div>
+</template>
+
+<script>
+export default {
+	emits:["titleClick"],
+	props:{
+		titles: {
+			type:	Array,
+			default() {
+				return []
+			}
+		}
+	},
+	data() {
+		return {
+			currentIndex: 0
+		}
+	},
+	methods: {
+		itemClick(index) {
+			this.currentIndex = index;
+			this.$emit("titleClick", index);
+		}
+	},
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+	.tab-control {
+		display: flex;
+	}
+	.tab-control-item {
+		flex: 1;
+		text-align: center;
+	}
+	.tab-control-item.active {
+		color: red;
+	}
+	.tab-control-item.active span {
+		border-bottom: 3px solid red;
+		padding: 5px 10px;
+	}
+</style>
