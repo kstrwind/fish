d7e68a9e3196a172d54a19f3e06ad27ef8d18fb4
kstrwind <kstrwind@163.com>
2024-03-27 11:38:44 +0800

    event bus pass

--- web/vue/chapter/03_event_bus/src/App.vue
+++ web/vue/chapter/03_event_bus/src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<div class="app">
+		App
+		<home-page/>
+		<about-event/>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomePage from './components/HomePage.vue';
+import AboutEvent from './components/AboutEvent.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomePage,
+		AboutEvent,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
